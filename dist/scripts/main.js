7916d511671ad93d997daf5d01b08e4c6d7e868b
Alexey Kabanets <akab@square-nest.com>
2019-06-15 01:23:48 +0300

    Added single product page & some fixes

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -7,16 +7,21 @@ function ready() {
 
     var body = document.body;
     var windowWidth = window.innerWidth || document.documentElement.clientWidth;
-    var cards = $('.js_category-item');
+    var cards = $('.js_cards');
+    var slider = $('.js_slider');
 
     /*==========
     events
     ==========*/
 
+    if (slider.length > 0) {
+        initSwiper();
+    }
+
     cards.each(function(idx, card) {
         let down;
         let up;
-        let link = $(card).find('a');
+        let link = $(card).find('a')[0];
 
         $(card).on('mousedown', function(event) {
             down = +new Date();
@@ -25,8 +30,7 @@ function ready() {
         $(card).on('mouseup', function(event) {
             up = +new Date();
             if ((up - down) < 200) {
-                console.log('click');
-                $(link).eq(0).click();
+                link.click();
 
             }
         });
@@ -35,4 +39,18 @@ function ready() {
     /*==========
     functions
     ==========*/
+
+    function initSwiper() {
+        var swiper = new Swiper(slider, {
+            pagination: {
+                el: '.slider-pagination',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.slider-button-next',
+                prevEl: '.slider-button-prev',
+            },
+            loop: true
+        });
+    }
 }
