7e361301008d20d6e350619bb2fb7cb20c07acb7
Alexey Kabanets <akab@square-nest.com>
2019-06-17 12:33:39 +0300

    finaly fixed desktop version

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -9,6 +9,7 @@ function ready() {
     var windowWidth = window.innerWidth || document.documentElement.clientWidth;
     var cards = $('.js_cards');
     var slider = $('.js_slider');
+    var formFields = $('.js_form__field');
 
     /*==========
     events
@@ -18,27 +19,37 @@ function ready() {
         initSwiper();
     }
 
-    cards.each(function(idx, card) {
-        let down;
-        let up;
-        let link = $(card).find('a')[0];
+    if (cards.length > 0) {
+        onCardClick();
+    }
 
-        $(card).on('mousedown', function(event) {
-            down = +new Date();
-        });
-
-        $(card).on('mouseup', function(event) {
-            up = +new Date();
-            if ((up - down) < 200) {
-                link.click();
-
-            }
-        });
-    });
+    if (formFields.length > 0) {
+        onFormFieldFocus();
+    }
 
     /*==========
     functions
     ==========*/
+
+    function onCardClick() {
+        cards.each(function(idx, card) {
+            let down;
+            let up;
+            let link = $(card).find('a')[0];
+
+            $(card).on('mousedown', function(event) {
+                down = +new Date();
+            });
+
+            $(card).on('mouseup', function(event) {
+                up = +new Date();
+                if ((up - down) < 200) {
+                    link.click();
+
+                }
+            });
+        });
+    }
 
     function initSwiper() {
         var swiper = new Swiper(slider, {
@@ -51,6 +62,18 @@ function ready() {
                 prevEl: '.slider-button-prev',
             },
             loop: true
+        });
+    }
+
+    function onFormFieldFocus() {
+        formFields.each(function(idx, field) {
+            $(field).change(function(event) {
+                if ($(this).val().length > 0 && $(this).val().trim() != '') {
+                    $(this).addClass('form__input-val');
+                } else {
+                    $(this).removeClass('form__input-val');
+                }
+            });
         });
     }
 }
